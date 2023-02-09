5a669b0e0996acef4244096587c2e05bdc22e3f4
Katorz <katorz.art@gmail.com>
2023-02-09 21:21:45 +0100

    Hook cetus cycle data to WarframeStat API

--- src/pages/CetusPage.tsx
+++ src/pages/CetusPage.tsx
@@ -1,5 +1,45 @@
-function CetusPage() {
-    return <>Cetus</>
+import { useEffect, useState } from "react";
+
+const API_ROUTE = "https://api.warframestat.us/pc/cetusCycle";
+
+function formatDuration(durationInMillis: number): string {
+  const str: (x: number) => string = (x) =>
+    x.toLocaleString("en-US", { minimumIntegerDigits: 2 });
+  const durationInSecs = Math.floor(durationInMillis / 1000);
+  const hours = Math.floor(durationInSecs / 3600);
+  const minutes = Math.floor(durationInSecs / 60) % 60;
+  const seconds = durationInSecs % 60;
+  return `${str(hours)}:${str(minutes)}:${str(seconds)}`;
 }
 
-export default CetusPage
+function CetusPage() {
+  const [cycle, setCycle] = useState("");
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    fetch(API_ROUTE)
+      .then((res) => res.json())
+      .then((data) => {
+        setCycle(data.isDay ? "Day" : "Night");
+        setTimer(Date.parse(data.expiry) - Date.now());
+      });
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1>Cetus</h1>
+        <div>
+          <p>Cycle</p>
+          <p>{cycle}</p>
+        </div>
+        <div>
+          <p>Ends in</p>
+          <p>{formatDuration(timer)}</p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default CetusPage;
