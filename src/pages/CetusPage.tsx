12ae6914093f08a7e9dee4fb3b07fb55c13056f1
Katorz <katorz.art@gmail.com>
2023-02-10 17:20:12 +0100

    Remove unused format time in page

--- src/pages/CetusPage.tsx
+++ src/pages/CetusPage.tsx
@@ -3,16 +3,6 @@ import OpenWorldHeader from "../components/OpenWorldHeader";
 
 const API_ROUTE = "https://api.warframestat.us/pc/cetusCycle";
 
-function formatDuration(durationInMillis: number): string {
-  const str: (x: number) => string = (x) =>
-    x.toLocaleString("en-US", { minimumIntegerDigits: 2 });
-  const durationInSecs = Math.floor(durationInMillis / 1000);
-  const hours = Math.floor(durationInSecs / 3600);
-  const minutes = Math.floor(durationInSecs / 60) % 60;
-  const seconds = durationInSecs % 60;
-  return `${str(hours)}:${str(minutes)}:${str(seconds)}`;
-}
-
 function CetusPage() {
   const [cycle, setCycle] = useState("");
   const [timer, setTimer] = useState(0);
